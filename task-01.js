2d6bf8951681092904cce27174f7b8317b811e04
Kirill Romanenko <brs.group.com@gmail.com>
2021-07-02 19:07:22 +0300

    finished 1 assignment

---
+++ task-01.js
@@ -0,0 +1,8 @@
+const ulCategories = document.querySelectorAll('.item');
+
+console.log(`В списке ${ulCategories.length} категории.`)
+
+const report = document.querySelectorAll('h2');
+
+
+console.log(report)
